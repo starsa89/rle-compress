77d33b3ce4e0ab5c3b5b0d2b69242f2c1f475e78
Sergey Startsev <startsev89@gmail.com>
2018-03-25 17:29:16 +0300

    add rle compress with space, sign, number

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /**
  * Реализовать RLE-сжатие: AAAB -> A3B, BCCDDDAXXXX -> BC2D3AX4
@@ -6,69 +6,93 @@
  * @return {string}
  */
 function rle(value) {
-  let arrayStr = value.slice();
-  let result = "";
-  let counter = 1;
+    let arrayStr = value.slice();
+    let result = "";
+    let counter = 1;
 
-  for (let i = 0; i < arrayStr.length; i++) {
-    let beforeLetter = arrayStr[i - 1];
-    let curLetter = arrayStr[i];
+    for (let i = 0; i < arrayStr.length; i++) {
+        let beforeLetter = arrayStr[i - 1];
+        let curLetter = arrayStr[i];
 
-    // Первый элемент
-    if (beforeLetter == undefined) {
-      continue;
-      // Последний элемент
-    } else if (i == arrayStr.length - 1) {
-      result += curLetter;
-      result += ++counter;
-    } else {
-      // Если нашли одинаковый элемент, увеличим счетчик
-      if (curLetter == beforeLetter) {
-        counter++;
-        // Если символы разные, но счетчик больше одного
-        // то значит надо сжать символы
-        // запишем предыдуший символ и счетчик
-      } else if (counter > 1) {
-        result += beforeLetter;
-        result += counter;
-        counter = 1;
-        // В проивном случае символ один, просто добавим
-      } else {
-        result += beforeLetter;
-        counter = 1;
-      }
-    }
-  }
-  return result;
-}
-function rerle(value) {
-  let arrayStr = value.slice();
-  let result = "";
+        // Первый элемент
+        if (beforeLetter === undefined) {
+            continue;
+            // Последний элемент
+        } else if (i == arrayStr.length - 1) {
+            result += curLetter;
+            result += ++counter;
+        } else {
+            // Если нашли одинаковый элемент, увеличим счетчик
+            if (curLetter == beforeLetter) {
+                counter++;
+                // Если символы разные, но счетчик больше одного
+                // то значит надо сжать символы
+                // запишем предыдуший символ и счетчик
+            } else if (counter > 1) {
 
-  for (let i = 0; i < arrayStr.length; i++) {
-    let beforeLetter = arrayStr[i - 1];
-    let curLetter = arrayStr[i];
-    // Первый элемент
-    if (beforeLetter == undefined) {
-      result += curLetter;
-    } else {
-      // Проверяем символ на число
-      let count = parseInt(curLetter);
-      if (count > 1) {
-        // Добавляем необходимое кол-во символов
-        for (let j = 1; j < count; j++) {
-          result += beforeLetter;
+                result += beforeLetter;
+                result += counter;
+                counter = 1;
+                // В проивном случае символ один, просто добавим
+            } else {
+                result += beforeLetter;
+                counter = 1;
+            }
         }
-      } else {
-        result += curLetter;
-      }
     }
-  }
-  return result;
+    return result;
 }
 
-console.log("  rle", "AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD");
+function rerle(value) {
+    let arrayStr = value.split('');
 
-let strZip = rle("AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD");
-console.log("zip: ", strZip);
-console.log("rerle", rerle(strZip));
+    let result = "";
+    let slice = false;
+    let insetCounter = false;
+    for (let i = 0; i < arrayStr.length; i++) {
+
+        let beforeLetter = arrayStr[i - 1];
+        let curLetter = arrayStr[i];
+
+        // Первый элемент
+        if (beforeLetter === undefined) {
+            result += curLetter;
+        } else {
+            let before = parseInt(beforeLetter);
+            let current = parseInt(curLetter);
+
+            let typeBefore = typeof(before);
+            let typeCurrent = typeof(current);
+
+            if ((typeCurrent == 'number' && !isNaN(current)) && (typeBefore == 'number' && !isNaN(before))) {
+
+                if (insetCounter) {
+                    result += curLetter;
+                    insetCounter = false;
+                    continue;
+                } else {
+                    // Добавляем необходимое кол-во символов
+                    for (let j = 1; j < current; j++) {
+                        result += beforeLetter;
+                    }
+                    insetCounter = true;
+                    continue;
+                }
+
+            }
+            // Проверяем символ на число
+            if (typeCurrent == 'number' && !isNaN(current)) {
+                // Добавляем необходимое кол-во символов
+                for (let j = 1; j < current; j++) {
+                    result += beforeLetter;
+                }
+                insetCounter = true;
+                continue;
+            } else {
+                result += curLetter;
+            }
+            insetCounter = false;
+        }
+    }
+    return result;
+}
